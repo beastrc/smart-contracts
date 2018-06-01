fdb9fb89c9e8cb8ed0cea777f97a009e2ef0ee25
beastrc <makarov0114@outlook.com>
2018-06-01 17:29:41 -0400

    Updates to Reputation

--- snowflake-poc/contracts/HydroReputation.sol
+++ snowflake-poc/contracts/HydroReputation.sol
@@ -8,75 +8,75 @@ contract Snowflake {
 
 contract HydroRepuation is Ownable {
 
-  address snowflakeAddress = 0x0;
+    address snowflakeAddress = 0x0;
 
-  struct AddressGroup {
-      address[] addressList;
-      mapping (address => bool) addressLookup;
-  }
+    struct AddressGroup {
+        address[] addressList;
+        mapping (address => bool) addressLookup;
+    }
 
-  function setSnowflakeAddress(address _address) public onlyOwner {
-      snowflakeAddress = _address;
-  }
+    function setSnowflakeAddress(address _address) public onlyOwner {
+        snowflakeAddress = _address;
+    }
 
-  mapping (address => Reputation) internal reputationList;
-  mapping (address => string[]) addedReputationsList;
-  mapping (address => mapping(string => uint)) addedReputationsLookup;
+    mapping (address => Reputation) internal reputationList;
+    mapping (address => string[]) addedReputationsList;
+    mapping (address => mapping(string => uint)) addedReputationsLookup;
 
-  Reputation[] public reputations;
-  mapping(address => uint256) internal addressToreputationId;
-  mapping(uint256 => uint256) internal snowflakeToReputationId;
+    AddressGroup[] addresses;
 
-  struct Reputation {
-      uint256 identityTokenId;
-      RepuationField[] repuationFieldsList;
-      mapping(string => RepuationField) repuationFieldsLookup;
-  }
+    struct Reputation {
+        uint256 identityTokenId;
+        RepuationField[] repuationFieldsList;
+        mapping(string => RepuationField) repuationFieldsLookup;
+    }
 
-  struct RepuationField {
-      string fieldName;
-      AddressGroup addresses;
-  }
+    struct RepuationField {
+        string fieldName;
+        uint256 addressGroupIndex;
+    }
 
-  function joinHydroRepuation() public {
-      Snowflake snowflake = Snowflake(snowflakeAddress);
-      uint256 tokenId = snowflake.ownerToToken(msg.sender);
-      reputationList[msg.sender].identityTokenId = tokenId;
-  }
+    function joinHydroRepuation() public {
+        Snowflake snowflake = Snowflake(snowflakeAddress);
+        uint256 tokenId = snowflake.ownerToToken(msg.sender);
+        reputationList[msg.sender].identityTokenId = tokenId;
+    }
 
-  function addReputationField(string _field) public {
-      require(addedReputationsLookup[msg.sender][_field] == 0,"");
+    function addReputationField(string _field) public {
+        require(addedReputationsLookup[msg.sender][_field] == 0,"");
 
-      uint256 id = addedReputationsList[msg.sender].push(_field);
-      addedReputationsLookup[msg.sender][_field] = id;
-      AddressGroup memory group;
-      reputationList[msg.sender].repuationFieldsList.push(RepuationField(_field, group));
-      reputationList[msg.sender].repuationFieldsLookup[_field].fieldName = _field;
-  }
+        uint256 id = addedReputationsList[msg.sender].push(_field);
+        addedReputationsLookup[msg.sender][_field] = id;
+        AddressGroup memory group;
+        uint256 addressListId = addresses.push(group) - 1;
+        reputationList[msg.sender].repuationFieldsList.push(RepuationField(_field, addressListId));
+        reputationList[msg.sender].repuationFieldsLookup[_field].fieldName = _field;
+        reputationList[msg.sender].repuationFieldsLookup[_field].addressGroupIndex = addressListId;
+    }
 
-  function attestToReputation(address _user, string _field) public {
-      require(addedReputationsLookup[_user][_field] > 0,"");
+    function attestToReputation(address _user, string _field) public {
+        require(addedReputationsLookup[_user][_field] > 0,"");
 
-      require(!reputationList[_user].repuationFieldsLookup[_field].addresses.addressLookup[msg.sender], "");
+        require(!addresses[reputationList[_user].repuationFieldsLookup[_field].addressGroupIndex].addressLookup[msg.sender], "");
 
-      uint256 id = addedReputationsLookup[_user][_field] - 1;
-      reputationList[_user].repuationFieldsList[id].addresses.addressList.push(msg.sender);
-      reputationList[_user].repuationFieldsList[id].addresses.addressLookup[msg.sender] = true;
-      reputationList[_user].repuationFieldsLookup[_field].addresses.addressList.push(msg.sender);
-      reputationList[_user].repuationFieldsLookup[_field].addresses.addressLookup[msg.sender] = true;
-  }
+        uint256 id = addedReputationsLookup[_user][_field] - 1;
+        uint addressId = reputationList[_user].repuationFieldsList[id].addressGroupIndex;
+        addresses[addressId].addressList.push(msg.sender);
+        addresses[addressId].addressLookup[msg.sender] = true;
+    }
 
-  function getReputation(address _user, string _field) public view returns(uint){
-      require(addedReputationsLookup[_user][_field] > 0,"");
-      return reputationList[_user].repuationFieldsLookup[_field].addresses.addressList.length;
-  }
+    function getReputation(address _user, string _field) public view returns(uint){
+        require(addedReputationsLookup[_user][_field] > 0,"");
+        uint addressId = reputationList[_user].repuationFieldsLookup[_field].addressGroupIndex;
+        return addresses[addressId].addressList.length;
+    }
 
-  function alreadyAttested(address _user, string _field) public view returns(bool){
-      return reputationList[_user].repuationFieldsLookup[_field].addresses.addressLookup[msg.sender];
-  }
+    function alreadyAttested(address _user, string _field) public view returns(bool){
+        return addresses[reputationList[_user].repuationFieldsLookup[_field].addressGroupIndex].addressLookup[msg.sender];
+    }
 
-  function addedRepLookup(address _user, string _field) public view returns(uint){
-      return addedReputationsLookup[_user][_field];
-  }
+    function addedRepLookup(address _user, string _field) public view returns(uint){
+        return addedReputationsLookup[_user][_field];
+    }
 
 }

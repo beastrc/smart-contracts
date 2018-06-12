4cb360051482302a8f826d863b6cee1371984ca9
beastrc <makarov0114@outlook.com>
2018-06-12 17:36:44 -0400

    Initial Tests for Address Ownership

--- snowflake-poc/contracts/resolvers/AddressOwnership.sol
+++ snowflake-poc/contracts/resolvers/AddressOwnership.sol
@@ -57,7 +57,7 @@ contract AddressOwnership is SnowflakeResolver {
         bool signed;
         bytes32 claimedSealedBid;
         while(!signed && (i < blockLag)) {
-            claimedSealedBid = keccak256(abi.encodePacked("Link Address to Snowflake", blockhash(block.number - ++i)));
+            claimedSealedBid = keccak256(abi.encodePacked("Link Address to Snowflake", blockhash(block.number - ++i), _address));
             signed = isSigned(_address, claimedSealedBid, v, r, s);
         }
         if (signed) {
